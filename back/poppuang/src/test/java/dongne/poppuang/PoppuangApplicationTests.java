b859aac5143ec9414cb5f0ed448a6b97ad396447
hardlypw <hardlypw@gmail.com>
2025-07-22 20:12:53 +0900

    기본구조

---
+++ back/poppuang/src/test/java/dongne/poppuang/PoppuangApplicationTests.java
@@ -0,0 +1,13 @@
+package dongne.poppuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
